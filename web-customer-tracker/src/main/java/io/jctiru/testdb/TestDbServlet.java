d07e019d1d75bfd13bb9858e17602a5cb6ccd657
jctiru <jonathanchristiantiru@gmail.com>
2019-08-31 13:19:43 +0800

    Add test for database connection

---
+++ web-customer-tracker/src/main/java/io/jctiru/testdb/TestDbServlet.java
@@ -0,0 +1,46 @@
+package io.jctiru.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String user = "springstudent";
+		String pass = "springstudent";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		PrintWriter out = response.getWriter();
+		out.println("Connecting to database: " + jdbcUrl);
+
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try (Connection con = DriverManager.getConnection(jdbcUrl, user, pass)) {
+			out.println("Connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+	}
+
+}
