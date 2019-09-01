7ee704c252e5996c7b3cd60ac8a28c14efa86144
jctiru <jonathanchristiantiru@gmail.com>
2019-09-01 18:00:20 +0800

    Inject dao to controller

--- web-customer-tracker/src/main/java/io/jctiru/webcustomertracker/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/io/jctiru/webcustomertracker/controller/CustomerController.java
@@ -1,15 +1,26 @@
 package io.jctiru.webcustomertracker.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.jctiru.webcustomertracker.dao.CustomerDAO;
+import io.jctiru.webcustomertracker.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		List<Customer> customers = customerDAO.getCustomers();
+		model.addAttribute("customers", customers);
 		return "list-customers";
 	}
 	
