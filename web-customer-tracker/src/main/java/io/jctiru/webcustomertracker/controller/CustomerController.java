54117044267f8728cabca8749664fced3ae8eccb
jctiru <jonathanchristiantiru@gmail.com>
2019-09-03 12:56:56 +0800

    Add search customer functionality

--- web-customer-tracker/src/main/java/io/jctiru/webcustomertracker/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/io/jctiru/webcustomertracker/controller/CustomerController.java
@@ -17,40 +17,48 @@ import io.jctiru.webcustomertracker.service.CustomerService;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
+
 	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		List<Customer> customers = customerService.getCustomers();
 		model.addAttribute("customers", customers);
 		return "list-customers";
 	}
-	
+
 	@GetMapping("/add-customer-form")
 	public String showFormForAdd(Model model) {
 		Customer customer = new Customer();
 		model.addAttribute("customer", customer);
 		return "customer-form";
 	}
-	
+
 	@PostMapping("/save-customer")
 	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
 		customerService.saveCustomer(customer);
 		return "redirect:/customer/list";
 	}
-	
+
 	@GetMapping("/update-customer-form")
 	public String showFormForUpdate(@RequestParam("customer-id") int id, Model model) {
 		Customer customer = customerService.getCustomer(id);
 		model.addAttribute("customer", customer);
 		return "customer-form";
 	}
-	
+
 	@GetMapping("/delete")
 	public String deleteCustomer(@RequestParam("customer-id") int id) {
 		customerService.deleteCustomer(id);
 		return "redirect:/customer/list";
 	}
+
+	@PostMapping("/search")
+	public String searchCustomers(@RequestParam("search-name") String searchName, Model model) {
+		List<Customer> customers = customerService.searchCustomers(searchName);
+		model.addAttribute("customers", customers);
+		return "list-customers";
+	}
+
 }
--- web-customer-tracker/src/main/java/io/jctiru/webcustomertracker/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/io/jctiru/webcustomertracker/dao/CustomerDAOImpl.java
@@ -45,4 +45,22 @@ public class CustomerDAOImpl implements CustomerDAO {
 		query.executeUpdate();
 	}
 
+	@Override
+	public List<Customer> searchCustomers(String searchName) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		Query query = null;
+
+		if (searchName != null && searchName.trim().length() > 0) {
+			query = currentSession.createQuery(
+					"from Customer where lower(firstName) like :searchName or lower(lastName) like :searchName",
+					Customer.class);
+			query.setParameter("searchName", "%" + searchName.toLowerCase() +"%");
+		} else {
+			query = currentSession.createQuery("from Customer", Customer.class);
+		}
+		
+		List<Customer> customers = query.getResultList();
+		return customers;
+	}
+
 }
